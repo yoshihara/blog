228939756c3763663f25de9ee6f925efe6e54bb6
yoshihara <yoshihara@users.noreply.github.com>
2018-07-28 21:48:30 +0900

    Fix the bug last commit date is shown as created date unexpectedly

--- bin/format_esa_md_for_hugo.sh
+++ bin/format_esa_md_for_hugo.sh
@@ -4,7 +4,7 @@
 # ファイル名にある ".html" というのを除いたblog.bomberowl.org/posts/#{id}/ というURLにしたいのでファイル名を変更している
 set -eux
 for file in `find ./content/posts/ -name "*.html.md"` ;do
-  first_commit_date=`git log -1 --reverse --oneline --pretty=format:'%ci' ${file}`
+  first_commit_date=`git log --reverse --oneline --pretty=format:'%ci' ${file} | head -1`
   sed -i s/\^created_at:.\\+$/"date: ${first_commit_date}"/ ${file}
   md=`echo ${file} | sed s/.html.md$/.md/`
   mv ${file} ${md}
