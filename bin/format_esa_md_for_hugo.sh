43d87e73e3127bf5cba5d6efacf960cb1fcd9be0
yoshihara <yoshihara@users.noreply.github.com>
2018-07-28 21:44:42 +0900

    Use first commit date for created date of post

--- bin/format_esa_md_for_hugo.sh
+++ bin/format_esa_md_for_hugo.sh
@@ -4,7 +4,8 @@
 # ファイル名にある ".html" というのを除いたblog.bomberowl.org/posts/#{id}/ というURLにしたいのでファイル名を変更している
 set -eux
 for file in `find ./content/posts/ -name "*.html.md"` ;do
-  sed -i s/\^created_at:/date:/ ${file}
+  first_commit_date=`git log -1 --reverse --oneline --pretty=format:'%ci' ${file}`
+  sed -i s/\^created_at:.\\+$/"date: ${first_commit_date}"/ ${file}
   md=`echo ${file} | sed s/.html.md$/.md/`
   mv ${file} ${md}
 done
